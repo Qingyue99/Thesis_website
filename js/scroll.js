5be8e79716aa163110214ba8bb65081c96a3cad6
Qingyue99 <qingyue@Qingyues-MacBook-Pro-2.local>
2019-03-13 13:17:15 -0400

    first commit

---
+++ js/scroll.js
@@ -0,0 +1,37 @@
+// the scrollopTop method gives us back a pixel value for how far it is to the top of the page
+$(document).on("scroll", function () {
+  
+  var pixels = $(document).scrollTop();
+  
+  $("div.progress div.bar").html(pixels + " pixels down")
+    
+  if (pixels < 125) {
+    $("header").removeClass("hide")
+  } else {
+    $("header").addClass("hide")
+  }
+  
+  if (pixels < 600) {
+    $("body").css("background-color", "#ffffff")
+  } else if (pixels < 1400) {
+    $("body").css("background-color", "#a29c97")
+  } else if (pixels < 2200) {
+    $("body").css("background-color", "#d9dfe4")
+  } else if (pixels < 3000) {
+    $("body").css("background-color", "#fff0f0")
+  } else {
+    $("body").css("background-color", "#cdccc7")
+  }
+  
+  var windowHeight = $(window).height()
+  var documentHeight = $(document).height()
+  var difference = documentHeight - windowHeight
+  
+  var percentage = 100 * pixels / difference
+  
+  $("div.progress div.bar").css("width", percentage + "%")
+
+  console.log('I have scrolled')
+  
+})
+
