02b3089c9d569dc2dbc8fce1d079c7d3de296a6a
Qingyue99 <qingyue@Qingyues-MacBook-Pro-2.local>
2019-03-21 23:05:59 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+var map;
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 42.335, lng: -71.015},
+      zoom: 12,
+      mapTypeId: 'satellite'
+    });
+    map.setTilt(45);
+  }
+
+function rotate90() {
+  var heading = map.getHeading() || 0;
+  map.setHeading(heading + 90);
+}
+
+function autoRotate() {
+  // Determine if we're showing aerial imagery.
+  if (map.getTilt() !== 0) {
+    window.setInterval(rotate90, 3000);
+  }
+}
+
+
 $('.menu-toggle').on('click', function(){
 
 $('.menu').toggleClass('open')
